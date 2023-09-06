a15814913756a7d941bde65fc594a37c895e3de3
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-09-05 21:15:55 -0300

    05/09/23
    Projeto cronometro (Situação de aprendizagem 3 bimestre)

--- FrontEnd/SituacapApendizagem2/Nova pasta/cronometro.js
+++ FrontEnd/SituacapApendizagem2/Nova pasta/cronometro.js
@@ -3,12 +3,14 @@
 const miliseg = document.querySelector('.milissegundos')
 const seg = document.querySelector('.segundos')
 const min = document.querySelector('.minutos')
+const hr = document.querySelector('.horas')
 
 //utilizando função let = função permite alterar seu valor 
 //declarando valores iniciais
 let miliNum = 0
 let segNum = 0
 let minNum = 0
+let hourNum = 0
 let INTERVALO
 
 //função miliseg, inicia seu valor constante até atingir "10" fazendo assim seu valor zerar.
@@ -28,7 +30,7 @@ function milissegundos() {
 
 function segundos() {
   segNum++
-  if (segNum < 10) {
+  if (segNum < 10) { //deixar 0 estatico na frente enquanto for menor que 10
     seg.innerHTML = '0' + segNum
   } else {
     seg.innerHTML = segNum
@@ -37,8 +39,11 @@ function segundos() {
   if (segNum == 59) {
     segNum = 0
     minutos()
+    }
   }
-}
+ 
+  
+
 
 function minutos() {
   minNum++
@@ -47,6 +52,22 @@ function minutos() {
   } else {
     min.innerHTML = minNum
   }
+
+  if (minNum == 59) {
+    minNum = 0
+    horas()
+  }
+}
+
+function horas() {
+  hourNum++
+  if (hourNum < 10) {
+    hr.innerHTML = '0' + hourNum
+  } else {
+    hr.innerHTML = hourNum
+  }
+
+  
 }
 
 function iniciar() {
@@ -65,7 +86,9 @@ function resetar() {
   miliNum = 0
   segNum = 0
   minNum = 0
+  hourNum = 0
   miliseg.innerHTML = '00'
   seg.innerHTML = '00'
   min.innerHTML = '00'
+  hr.innerHTML = '00'
 }
