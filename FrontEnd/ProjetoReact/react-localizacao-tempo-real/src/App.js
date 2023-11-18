5a1495f04cb90d3749fa78489f326fa6b929bb12
MuriloFiFer <123783941+MuriloFiFer@users.noreply.github.com>
2023-11-17 21:48:53 -0300

    17/11/23

---
+++ FrontEnd/ProjetoReact/react-localizacao-tempo-real/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import MapaAtual from './Mapa/MapaCidade';
+// Define o componente principal da aplicação, chamado 'App'.
+function App() {
+return (
+
+<div className="MapaAtual">
+<MapaAtual />
+</div>
+);
+}
+// Exporta o componente 'App' como o componente principal da aplicação.
+export default App;
